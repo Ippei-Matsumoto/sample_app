baae6dbbfeafb4d25f9259deaceec723194eef5c
Ippei-Matsumoto <i_ilsea3918syou@yahoo.co.jp>
2014-11-11 17:11:25 +0900

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#return perfect title about each page
+	def full_title(page_title)								# definition of method
+		base_title = "Ruby on Rails Tutorial Sample App"	# allocate value to valuables
+		if page_title.empty?								# test logical value
+			base_title										# retrun
+		else
+			"#{base_title} | #{page_title}"					#spread of strings("Shikitenkai")
+		end
+	end
 end
